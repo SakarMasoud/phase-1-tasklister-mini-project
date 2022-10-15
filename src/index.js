6f36eaa662414bf4532f5b34b00c84a5934ec6fc
Sakar Masoud <masoudsakar@gmail.com>
2022-10-15 10:37:05 +0300

    Sakar Masoud

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    createLi(e.target.newTask.value)
+    form.reset()
+  })
 });
+
+
+function createLi(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+
+  btn.addEventListener('click', handleD)
+  btn.textContent = 'x'
+
+  li.textContent= `${todo} `
+  li.appendChild(btn)
+
+  console.log(li)
+  document.querySelector('#tasks').appendChild(li)
+}
+
+function handleD(e){
+  e.target.parentNode.remove()
+}
